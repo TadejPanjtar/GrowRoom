6bf90cbfe70baf5cc9474217b9d3932ae9ad2698
Tadej Panjtar <tadej.panjtar@gmail.com>
2015-03-10 16:28:59 +0100

    relays

--- growRoom.py
+++ growRoom.py
@@ -1,10 +1,28 @@
 import RPi.GPIO as GPIO
+import am2302_ths as ths
 
+RelayON  = False  # relay are driven inverted
+RelayOFF = True
 
-ReleON  = False
-ReleOFF = True
-Rlight  = 27 # rele 1 - light
+# Wiring
+Rlight  = 27 # relay 1 - light
+Rpomp   = 22 # relay 2 - pomp
+Rheat   = 23 # relay 3 - heat
+Rpomp2  = 24 # relay 4 - pomp2
+THS     = 25 # temperature humidity sensor
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Rlight, GPIO.OUT)
-#GPIO.setup(Rlight, ReleOFF)
+#GPIO.setup(Rlight, RelayOFF)
+
+t=None; h=None;
+while not(t): t=ths.get_temperature(THS);
+while not(h): h=ths.get_humidity(THS);
+if t: # handle empty variable
+  temperature=t
+if h:
+  humidity=h
+#print 'temp',temperature, 'hum', humidity
+print 'Temp={0:0.1f}*C Humidity={1:0.1f}%' .format(temperature, humidity)
+
+
