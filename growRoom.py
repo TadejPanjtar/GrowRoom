4e1b34f5eed8850fe488e3b6b86bd335870a016b
Tadej Panjtar <tadej.panjtar@gmail.com>
2015-04-18 22:49:11 +0200

    merge from worker.py

--- growRoom.py
+++ growRoom.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import am2302_ths as ths
-
+import os
 from time import strftime,sleep
 from stgs import stgs
 
-
+current_dir = os.path.dirname(os.path.abspath(__file__))
 NumSensors = 3
 lightOldOn=False # read light status
 LIGHT_HOURS_LEAP=24
@@ -15,11 +15,54 @@ temperature=0
 humidity=0
 moisture=0
 LOGGING=True
+logs=[]
+LOGS_SIZE=55
 heating=False
 
 def log(msg):
-# if LOGGING:  print strftime("%Y-%m-%d %X"), msg
-  if LOGGING:  print strftime("%d-%X"), msg
+  # if LOGGING:  print strftime("%Y-%m-%d %X"), msg
+    if LOGGING:
+      logs.append(strftime("%d-%X")+" "+ msg)
+      if (len(logs)>LOGS_SIZE): del logs[0]
+
+def gv(v): # get value var. or atribute US: gv('c.sub.s.a') --> 111
+  i=v.rfind('.')
+  if i>0:
+    var= eval(v[:i])
+    return getattr(var, v[i+1:])
+  else:
+    return eval(v)
+
+def sv(v, newval): # set value var. or atribute US: gv('c.sub.s.a', 111)
+  i=v.rfind('.')
+  if i>0:
+    var= eval(v[:i])
+    try: setattr(var, v[i+1:], eval(newval))
+    except: setattr(var, v[i+1:], newval)
+  else:
+      try:
+          xy=eval(newval)+0
+          globals()[v]=xy
+      except:
+          globals()[v]=newval
+
+# Delete first line from file, which is returned
+def getFirstLine(fname):
+  if os.path.getsize(fname) > 0: # if have nonempty file
+    f = open(fname, 'r')
+    fw = open(fname+'.tmp' ,'w')
+    first=''
+    try:
+      all_lines = f.readlines(); first=all_lines[0];
+      for line in all_lines[1:]:
+        fw.write(line)
+    except: pass
+    f.close()
+    fw.close()
+    os.rename(fname+'.tmp',fname)
+    return first
+  else:
+    return ""
 
 RelayON  = False  # relay are driven inverted
 RelayOFF = True
@@ -105,7 +148,7 @@ def handle_pomp2():
     GPIO.setup(Rpomp2, RelayOFF)
 
 def tenMinutesCheck():
-  global tenMinutesRun, blinkTick
+  global tenMinutesRun
   tenMinutes=(strftime("%M")[1]=='0') # every 10 minutes
   if tenMinutesRun!=tenMinutes:
     if tenMinutes: 
@@ -113,12 +156,11 @@ def tenMinutesCheck():
       handle_heating()
       handle_pomp()
       handle_pomp2()
-    blinkTick=0
   tenMinutesRun=tenMinutes
 
 t=None; h=None; moisture=False;
 
-while True: # Main loop end
+while True: # Main loop begin
   if blinkTick % NumSensors == 0:
     t=ths.get_temperature(THS);
     if t: # handle empty variable
@@ -143,6 +185,24 @@ while True: # Main loop end
     GPIO.setup(LED3, LedOFF)
     GPIO.setup(LED4, LedOFF)
   blinkTick=blinkTick+1
+  if blinkTick>9223372036854775807: blinkTick=0
+  cmd=getFirstLine(current_dir+"/pipe").strip() # read commands and variables set from other process
+  if len(cmd)>0:
+    if cmd.find('=')<0:
+      if cmd=='logs':
+        logs_file = open(current_dir+"/logs", "w")
+        logs_file.write('\n'.join(logs) + '\n')
+        logs_file.close()
+        print("writing to file")
+      else:
+        try:
+          log(cmd.strip()+"="+str(gv(cmd.strip())))
+        except: pass
+    else:
+      try:
+        sv(cmd[:cmd.find('=')].strip(), cmd[1+cmd.find('='):].strip())
+        if "stgs.lightDuration"==cmd[:cmd.find('=')].strip(): handle_light()
+      except: print "no such var, :", cmd[:cmd.find('=')]
   tenMinutesCheck()
   # Main loop end
 
